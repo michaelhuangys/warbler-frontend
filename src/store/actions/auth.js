dccd8dedc756699c2251b4ae4db07b2c49e6f8a2
huang <yhuang@mai.sfsu.edu>
2018-04-25 15:10:43 -0700

    port

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,6 +1,7 @@
 import { apiCall, setTokenHeader } from "../../services/api";
 import { SET_CURRENT_USER } from "../actionTypes";
 import { addError, removeError } from "./errors";
+const APIURL='https://warbler-backend.herokuapp.com'
 
 export function setCurrentUser(user) {
   return {
@@ -25,7 +26,7 @@ export function authUser(type, userData) {
   return dispatch => {
     // wrap our thunk in a promise so we can wait for the API call
     return new Promise((resolve, reject) => {
-      return apiCall("post", `/api/auth/${type}`, userData)
+      return apiCall("post", `${APIURL}/api/auth/${type}`, userData)
         .then(({ token, ...user }) => {
           localStorage.setItem("jwtToken", token);
           setAuthorizationToken(token);
--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -1,7 +1,7 @@
 import { apiCall } from "../../services/api";
 import { addError } from "./errors";
 import { LOAD_MESSAGES, REMOVE_MESSAGE } from "../actionTypes";
-
+const APIURL='https://warbler-backend.herokuapp.com'
 export const loadMessages = messages => ({
   type: LOAD_MESSAGES,
   messages
@@ -14,7 +14,7 @@ export const remove = id => ({
 
 export const removeMessage = (user_id, message_id) => {
   return dispatch => {
-    return apiCall("delete", `https://warbler-backend.herokuapp.com/api/users/${user_id}/messages/${message_id}`)
+    return apiCall("delete", `${APIURL}/${user_id}/messages/${message_id}`)
       .then(() => dispatch(remove(message_id)))
       .catch(err => {
         addError(err.message);
@@ -24,7 +24,7 @@ export const removeMessage = (user_id, message_id) => {
 
 export const fetchMessages = () => {
   return dispatch => {
-    return apiCall("get", "/api/messages")
+    return apiCall("get", APIURL+"/api/messages")
       .then(res => {
         dispatch(loadMessages(res));
       })
@@ -37,7 +37,7 @@ export const fetchMessages = () => {
 export const postNewMessage = text => (dispatch, getState) => {
   let { currentUser } = getState();
   const id = currentUser.user.id;
-  return apiCall("post", `/api/users/${id}/messages`, { text })
+  return apiCall("post", `${APIURL}/api/users/${id}/messages`, { text })
     .then(res => {})
     .catch(err => addError(err.message));
 };
